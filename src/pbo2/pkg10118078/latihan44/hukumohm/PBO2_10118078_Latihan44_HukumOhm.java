e6ef51a45531b884ac67f184e9ae36dec483edaa
Steven Danesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-11-07 17:52:08 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118078/latihan44/hukumohm/PBO2_10118078_Latihan44_HukumOhm.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118078.latihan44.hukumohm;
+
+/**
+ *
+ * @author  
+ * Nama     : Steven Danesswaralay
+ * Kelas    : IF-2
+ * Nim      : 10118078
+ * Deskripsi Tugas : Hukum Ohm 
+ */
+public class PBO2_10118078_Latihan44_HukumOhm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Baterai();
+        Baterai baterai = new Baterai(3,12);
+        
+    }
+    
+}
